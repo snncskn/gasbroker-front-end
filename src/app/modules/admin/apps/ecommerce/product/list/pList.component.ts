9af31e5017a119bc6e11f0a8672ab47398f2b688
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2021-07-08 17:31:47 +0300

    fixed product details

--- src/app/modules/admin/apps/ecommerce/product/list/pList.component.ts
+++ src/app/modules/admin/apps/ecommerce/product/list/pList.component.ts
@@ -34,7 +34,7 @@ export class InventoryListComponent implements OnInit, AfterViewInit, OnDestroy
     isLoading: boolean = false;
     pagination: InventoryPagination;
     productsCount: number = 0;
-    productsTableColumns: string[] = ['color','product_name','product_code', 'minute','price', 'active', 'details'];
+    productsTableColumns: string[] = ['name','code', 'active', 'details'];
     searchInputControl: FormControl = new FormControl();
     selectedProduct: InventoryProduct | null = null;
     selectedProductForm: FormGroup;
@@ -62,30 +62,9 @@ export class InventoryListComponent implements OnInit, AfterViewInit, OnDestroy
         // Create the selected product form
         this.selectedProductForm = this._formBuilder.group({
             id               : [''],
-            product_name     : [''], 
-            color            : [''], 
-            product_code     : ['MAN & WOMAN'], 
-            time_break       : [],
-            minute           : [''], 
-            aop_price        : [''], 
-            description      : [''],
-            tags             : [[]],
-            sku              : [''],
-            barcode          : [''],
-            brand            : [''],
-            vendor           : [''],
-            stock            : [''],
-            reserved         : [''],
-            cost             : [''],
-            basePrice        : [''],
-            taxPercent       : [''],
-            price            : [''],
-            aopPrice         : [''],
-            weight           : [''],
-            thumbnail        : [''],
-            images           : [[]],
-            currentImageIndex: [0], // Image index that is currently being viewed
-            active           : [false]
+            name     : [''], 
+            code     : ['GAS & OIL & YAĞĞĞĞ'], 
+            active           : [true]
         });
 
         this.isLoading=true;
@@ -174,7 +153,7 @@ export class InventoryListComponent implements OnInit, AfterViewInit, OnDestroy
                 switchMap((query) => {
                     this.closeDetails();
                     this.isLoading = true;
-                    return this._inventoryService.getProducts(1, 10, 'product_name', 'asc', query);
+                    return this._inventoryService.getProducts(1, 10, 'name', 'asc', query);
                 }),
                 map(() => {
                     this.isLoading = false;
@@ -241,7 +220,7 @@ export class InventoryListComponent implements OnInit, AfterViewInit, OnDestroy
         // Get the product by id
         this._inventoryService.getProductById(productId)
             .subscribe((product) => {
-
+                console.log("12345");
                 // Set the selected product
                 this.selectedProduct = product;
 
@@ -261,30 +240,30 @@ export class InventoryListComponent implements OnInit, AfterViewInit, OnDestroy
         this.selectedProduct = null;
     }
 
-    /**
-     * Cycle through images of selected product
-     */
-    cycleImages(forward: boolean = true): void
-    {
-        // Get the image count and current image index
-        const count = this.selectedProductForm.get('images').value.length;
-        const currentIndex = this.selectedProductForm.get('currentImageIndex').value;
+    // /**
+    //  * Cycle through images of selected product
+    //  */
+    // cycleImages(forward: boolean = true): void
+    // {
+    //     // Get the image count and current image index
+    //     const count = this.selectedProductForm.get('images').value.length;
+    //     const currentIndex = this.selectedProductForm.get('currentImageIndex').value;
 
-        // Calculate the next and previous index
-        const nextIndex = currentIndex + 1 === count ? 0 : currentIndex + 1;
-        const prevIndex = currentIndex - 1 < 0 ? count - 1 : currentIndex - 1;
+    //     // Calculate the next and previous index
+    //     const nextIndex = currentIndex + 1 === count ? 0 : currentIndex + 1;
+    //     const prevIndex = currentIndex - 1 < 0 ? count - 1 : currentIndex - 1;
 
-        // If cycling forward...
-        if ( forward )
-        {
-            this.selectedProductForm.get('currentImageIndex').setValue(nextIndex);
-        }
-        // If cycling backwards...
-        else
-        {
-            this.selectedProductForm.get('currentImageIndex').setValue(prevIndex);
-        }
-    }
+    //     // If cycling forward...
+    //     if ( forward )
+    //     {
+    //         this.selectedProductForm.get('currentImageIndex').setValue(nextIndex);
+    //     }
+    //     // If cycling backwards...
+    //     else
+    //     {
+    //         this.selectedProductForm.get('currentImageIndex').setValue(prevIndex);
+    //     }
+    // }
 
     /**
      * Toggle the tags edit mode
@@ -356,22 +335,22 @@ export class InventoryListComponent implements OnInit, AfterViewInit, OnDestroy
     /**
      * Delete the selected product using the form data
      */
-    deleteSelectedProduct(): void
-    {
-        // Get the product object
-        const product = this.selectedProductForm.getRawValue();
+    // deleteSelectedProduct(): void
+    // {
+    //     // Get the product object
+    //     const product = this.selectedProductForm.getRawValue();
 
-        // Delete the product on the server
-        this.ngxService.start();
+    //     // Delete the product on the server
+    //     this.ngxService.start();
 
-        this._inventoryService.deleteProduct(product.id).subscribe(() => {
+    //     this._inventoryService.deleteProduct(product.id).subscribe(() => {
 
-            // Close the details
-            this.ngxService.stop();
+    //         // Close the details
+    //         this.ngxService.stop();
 
-            this.closeDetails();
-        });
-    }
+    //         this.closeDetails();
+    //     });
+    // }
 
     /**
      * Show flash message
@@ -418,12 +397,12 @@ export class InventoryListComponent implements OnInit, AfterViewInit, OnDestroy
     onSearchChange(searchValue: string): void {  
         this.products$.forEach(item=>{
             item.forEach(it=>{
-               if(it.product_name.indexOf(searchValue)>0){
+               if(it.name.indexOf(searchValue)>0){
                     merge(searchValue).pipe(
                         switchMap(() => {
                             this.closeDetails();
                             this.isLoading = true;
-                            return this._inventoryService.getProducts(0,10,'product_name',"asc",searchValue);
+                            return this._inventoryService.getProducts(0,10,'name',"asc",searchValue);
                         }),
                         map(() => {
                             this.isLoading = false;
