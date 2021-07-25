cc5023bac6806d07e863e8dbc51368bfd8a70632
Uzeyr OZCAN <muzeyr@gmail.com>
2021-07-25 15:36:43 +0300

    a

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
     production: true,
-    url: 'http://localhost:3300'
+  //  url: 'https://localhost:3300',
+    url: 'https://3.122.107.144:3300'
+    //url:'https://ec2-3-122-107-144.eu-central-1.compute.amazonaws.com'
+    
+
 
 };
