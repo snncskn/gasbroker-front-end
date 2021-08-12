b682ac4c9f25ee7c16cbe486d42a443069180dbf
M Uzeyr OZCAN <alsactolga@hotmail.com>
2021-08-12 12:28:17 +0300

    docs delete button added

--- src/app/modules/admin/apps/vehicles/details/vehiclesDetails.component.ts
+++ src/app/modules/admin/apps/vehicles/details/vehiclesDetails.component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FileUploadComponent } from '@iplab/ngx-file-upload';
 import { TranslocoService } from '@ngneat/transloco';
+import { AuthService } from 'app/core/auth/auth.service';
 import { FileService } from 'app/services/file.service';
 import { GeneralFunction } from 'app/shared/GeneralFunction';
 import { environment } from 'environments/environment';
@@ -10,6 +12,7 @@ import { ToastrManager } from 'ng6-toastr-notifications';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { ConfirmationDialog } from '../../delete-dialog/delete.component';
+import { MediaService } from '../../media/media.service';
 import { VehiclesService } from '../vehicles.service';
 
 @Component({
@@ -19,6 +22,8 @@ import { VehiclesService } from '../vehicles.service';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class VehiclesDetailsComponent implements OnInit {
+
+  @ViewChild('fileUpload')fileUpload: FileUploadComponent;
 
     dialogRef: MatDialogRef<ConfirmationDialog>;
     public generalFunction = new GeneralFunction();
@@ -32,6 +37,7 @@ export class VehiclesDetailsComponent implements OnInit {
     selectCustomerItem: any;
     dataSourceDocs: any[];
     fileDownloadLink:any;
+    isLoadDocs: boolean = true;
 
 
   //file upload
@@ -56,9 +62,10 @@ export class VehiclesDetailsComponent implements OnInit {
         private fileService: FileService,
         private readonly activatedRouter: ActivatedRoute,
         private translocoService: TranslocoService,
-        private dialog: MatDialog
-
-
+        private dialog: MatDialog,
+        private mediaService: MediaService,
+        private authService: AuthService,
+        private changeDetection: ChangeDetectorRef,
 
     ) {
         this.list();
@@ -72,18 +79,18 @@ export class VehiclesDetailsComponent implements OnInit {
         });
         
      
-
+      this._vehicleService.getTypes().subscribe(res => {
+          this.dataSourceTypes = res.body;
+      });
+      this._vehicleService.getVehicleDocs().subscribe(res => {
+          this.dataSourceDocs = res.body;
+      });
         
     }
 
     ngOnInit(): void {
 
-        this._vehicleService.getTypes().subscribe(res => {
-            this.dataSourceTypes = res.body;
-        });
-        this._vehicleService.getVehicleDocs().subscribe(res => {
-            this.dataSourceDocs = res.body;
-        });
+
     }
 
     addNewVehicle() {
@@ -151,6 +158,20 @@ export class VehiclesDetailsComponent implements OnInit {
         );
         });
     }
+    loadDocs() {
+      this.isLoadDocs = false;
+      this.mediaList = [];
+      this.activatedRouter.paramMap.subscribe(params => {
+        if (params.has('id')) {
+          this._vehicleService.getVehicleById(params.get("id")).subscribe(data => {
+                this.mediaList = data?.body.media;
+                this.changeDetection.detectChanges();
+            })
+        };
+      });
+      this.fileUpload.control.clear()
+      this.isLoadDocs = true;
+    }
 
     selectCustomer(event: any) {
         let option = this.customers.filter(item => item.full_name.toUpperCase() === event.option.value.full_name.toUpperCase());
@@ -178,6 +199,21 @@ export class VehiclesDetailsComponent implements OnInit {
               });
         }
     }
+    deleteDocs(item) {
+      if (item) {
+        this.dialogRef = this.dialog.open(ConfirmationDialog, {
+          disableClose: false,
+        });
+        this.dialogRef.afterClosed().subscribe((result) => {
+          if (result) {
+            this._vehicleService.deleteDocs(item).subscribe(data => {
+              this.loadDocs();
+            })
+          }
+          this.dialogRef = null;
+        });
+      }
+    }
 
     public toggleStatus(): void {
         this.filesControl.disabled
@@ -193,19 +229,55 @@ export class VehiclesDetailsComponent implements OnInit {
         this.filesControl.setValue([]);
       }
     
-      upload() {
+      upload(item) {
         const file = this.demoForm.value.files[0];
         this.fileService.putUrl(file).then((res) => {
           const {
             data: { putURL },
           } = res;
     
-          this.fileService.upLoad(putURL, file).then((res) => {
-            this.toastr.warningToastr("Yuppii...", "Success!");
-            // this.mediaService.create({id:null,company_id: this.companyDetail, title: ret.putURL}).subscribe((data) => { });
-          });
+          this.fileService.upLoad(putURL, file).then(
+            (res) => {
+              this.toastr.successToastr(
+                this.translocoService.translate("message.fileUpload")
+              );
+    
+              let key = this.authService.user_id + '/'+ file.name;
+              let pathObject = {type:file.type, fileName:file.name, key:key, group: item.description}
+              this.mediaService
+                .createMedia({
+                  id: null,
+                  vehicle_id: this.vehicleDetail,
+                  title: "VehicleFile",
+                  user_id: this.authService.user_id,
+                  path: JSON.stringify(pathObject),
+                })
+                .subscribe((data) => {
+                  this.toastr.successToastr(
+                    this.translocoService.translate("message.createMedia"));
+                    this.loadDocs();
+                });
+            },
+            (error) => {
+              this.toastr.errorToastr(
+                this.translocoService.translate("message.error")
+              );
+              this.loadDocs();
+            }
+          );
         });
       }
+
+        /**
+        * Track by function for ngFor loops
+        *
+        * @param index
+        * @param item
+        */
+       trackByFn(index: number, item: any): any
+       {
+           return item.id || index;
+       }
       clickFile(item)
       {
         this.fileDownloadLink = `${environment.url}/media/s3/generateGetUrl?Key=`+item?.path.key;
