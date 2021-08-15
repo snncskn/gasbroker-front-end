fc5ac54213af142ef9cdbb0081ff9dc95c238a9e
Sinan COSKUN <snncskn@msn.com>
2021-08-15 21:26:24 +0300

    env changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-   // url: 'https://gasbroker.herokuapp.com'
-    url: 'http://localhost:3300'
+    url: 'https://gasbroker.herokuapp.com'
+    //url: 'http://localhost:3300'
 };
