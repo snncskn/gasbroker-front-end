807828f316da26ffebd3b96d590104847f64076f
tolgaalsac <alsactolga@hotmail.com>
2021-08-26 17:16:39 +0300

    update

--- src/app/modules/admin/apps/vehicles/details/vehiclesDetails.component.ts
+++ src/app/modules/admin/apps/vehicles/details/vehiclesDetails.component.ts
@@ -17,29 +17,30 @@ import { MediaService } from '../../media/media.service';
 import { VehiclesService } from '../vehicles.service';
 
 @Component({
-    selector: 'vehiclesDetails',
-    templateUrl: './vehiclesDetails.component.html',
-    encapsulation: ViewEncapsulation.None,
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'vehiclesDetails',
+  templateUrl: './vehiclesDetails.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class VehiclesDetailsComponent implements OnInit {
 
-  @ViewChild('fileUpload')fileUpload: FileUploadComponent;
+  @ViewChild('fileUpload') fileUpload: FileUploadComponent;
 
-    dialogRef: MatDialogRef<ConfirmationDialog>;
-    public generalFunction = new GeneralFunction();
+  dialogRef: MatDialogRef<ConfirmationDialog>;
+  public generalFunction = new GeneralFunction();
 
-    vehicleForm: FormGroup;
-    dataSourceTypes: any[];
-    vehicleDetail: string;
-    customers: any[];
-    mediaList: any[];
-    filteredOptions: Observable<string[]>;
-    selectCustomerItem: any;
-    dataSourceDocs: any[];
-    fileDownloadLink:any;
-    isLoadDocs: boolean = true;
-    isLoading: boolean = false;
+  vehicleForm: FormGroup;
+  dataSourceTypes: any[];
+  vehicleDetail: string;
+  customers: any[];
+  mediaList: any[];
+  filteredOptions: Observable<string[]>;
+  selectCustomerItem: any;
+  dataSourceDocs: any[];
+  fileDownloadLink: any;
+  isLoadDocs: boolean = true;
+  isLoading: boolean = false;
+  imgsrc: any;
 
 
   //file upload
@@ -55,239 +56,298 @@ export class VehiclesDetailsComponent implements OnInit {
   //file upload
 
 
-    constructor(
-        private router: Router,
-        private _formBuilder: FormBuilder,
-        private _vehicleService: VehiclesService,
-        public toastr: ToastrManager,
-        private _router: Router,
-        private fileService: FileService,
-        private readonly activatedRouter: ActivatedRoute,
-        private translocoService: TranslocoService,
-        private dialog: MatDialog,
-        private mediaService: MediaService,
-        private authService: AuthService,
-        private changeDetection: ChangeDetectorRef,
-        private readonly ngxService: NgxUiLoaderService,
+  constructor(
+    private router: Router,
+    private _formBuilder: FormBuilder,
+    private _vehicleService: VehiclesService,
+    public toastr: ToastrManager,
+    private _router: Router,
+    private fileService: FileService,
+    private readonly activatedRouter: ActivatedRoute,
+    private translocoService: TranslocoService,
+    private dialog: MatDialog,
+    private mediaService: MediaService,
+    private authService: AuthService,
+    private changeDetection: ChangeDetectorRef,
+    private readonly ngxService: NgxUiLoaderService,
 
-    ) {
-      this.ngxService.start();
-        this.list();
-        this.vehicleForm = this._formBuilder.group({
-            id: [''],
-            company_id: [''],
-            company: ['',[Validators.required]],
-            name: ['', [Validators.required]],
-            type: ['',[Validators.required]],
-            registered_date: [null,[Validators.required]],
-        });
-        
-     
-      this._vehicleService.getTypes().subscribe(res => {
-          this.dataSourceTypes = res.body;
-      });
-      this._vehicleService.getVehicleDocs().subscribe(res => {
-          this.dataSourceDocs = res.body;
-      });
-    this.ngxService.stop();  
+  ) {
+    this.ngxService.start();
+    this.list();
+    this.vehicleForm = this._formBuilder.group({
+      id: [''],
+      company_id: [''],
+      company: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      type: ['', [Validators.required]],
+      registered_date: [null, [Validators.required]],
+    });
+
+
+    this._vehicleService.getTypes().subscribe(res => {
+      this.dataSourceTypes = res.body;
+    });
+    this._vehicleService.getVehicleDocs().subscribe(res => {
+      this.dataSourceDocs = res.body;
+    });
+    this.ngxService.stop();
+  }
+
+  ngOnInit(): void {
+
+
+  }
+
+  addNewVehicle() {
+    let status = this.generalFunction.formValidationCheck(this.vehicleForm, this.toastr, this.translocoService);
+    if (status) {
+      return
     }
 
-    ngOnInit(): void {
+    let createVehicle = {
+      id: '', type: '', name: '', company_id: '', registered_date: ''
+    };
 
+    createVehicle.id = this.vehicleDetail;
+    createVehicle.type = this.vehicleForm.value.type;
+    createVehicle.name = this.vehicleForm.value.name;
+    createVehicle.company_id = this.vehicleForm.value.company_id;
+    createVehicle.registered_date = this.vehicleForm.value.registered_date;
 
+    if (createVehicle.company_id === '') {
+      this.toastr.errorToastr(this.translocoService.translate('message.requiredCompany'));
+      return;
+    }
+    this._vehicleService.createVehicle(createVehicle).subscribe(data => {
+      this._router.navigateByUrl('/apps/vehicles/list');
+    });
+  }
+
+  private _filter(value: string): string[] {
+
+    let filterValue;
+    if (value === '99') {
+      filterValue = '';
+    } else {
+      filterValue = value;
     }
 
-    addNewVehicle() {
-        let status = this.generalFunction.formValidationCheck(this.vehicleForm,this.toastr,this.translocoService);
-        if(status)
-        {
-          return
-        }
-        
-        let createVehicle = {
-            id: '', type: '', name: '', company_id: '', registered_date: ''};
+    return this.customers.filter(option => {
+      if (typeof filterValue === 'object') {
+        return option?.full_name?.indexOf(filterValue.full_name) === 0 || option?.full_name?.indexOf(filterValue.full_name?.toLowerCase()) === 0;
 
-        createVehicle.id = this.vehicleDetail;
-        createVehicle.type = this.vehicleForm.value.type;
-        createVehicle.name = this.vehicleForm.value.name;
-        createVehicle.company_id = this.vehicleForm.value.company_id;
-        createVehicle.registered_date = this.vehicleForm.value.registered_date;
+      } else {
+        return option?.full_name?.indexOf(filterValue) === 0 || option?.full_name?.indexOf(filterValue?.toLowerCase()) === 0;
 
-        if(createVehicle.company_id ===''){
-            this.toastr.errorToastr(this.translocoService.translate('message.requiredCompany'));
-            return;
-        }
-        this._vehicleService.createVehicle(createVehicle).subscribe(data => {
-        this._router.navigateByUrl('/apps/vehicles/list');
-        });
-    }
+      }
+    });
+  }
 
-    private _filter(value: string): string[] {
-
-        let filterValue;
-        if (value === '99') {
-          filterValue = '';
-        } else {
-          filterValue = value;
-        }
-    
-        return this.customers.filter(option => {
-            if( typeof filterValue === 'object'){
-                return option?.full_name?.indexOf(filterValue.full_name) === 0 || option?.full_name?.indexOf(filterValue.full_name?.toLowerCase()) === 0;
-
-            }else{
-                return option?.full_name?.indexOf(filterValue) === 0 || option?.full_name?.indexOf(filterValue?.toLowerCase()) === 0;
-
-            }
-        });
-    }
-
-    public list() {
-        this._vehicleService.getCustomers().subscribe(data => {
-          this.customers = data.body;
-          this.activatedRouter.paramMap.subscribe(params => {
-            if (params.has('id')) {
-                this._vehicleService.getVehicleById(params.get("id")).subscribe(data=>{
-                    this.vehicleDetail = data.body.id;
-                    this.vehicleForm.patchValue(data.body);
-                    this.vehicleForm.value.company_id = data.body.company.id;
-                    this.vehicleForm.value.company = data.body.company.name;
-                    this.mediaList = data?.body.media;
-                }) 
-            };
-        });
-        this.filteredOptions = this.vehicleForm.controls['company'].valueChanges.pipe(
-        startWith(''),
-        map(value => this._filter(value === '' ? '99' : value))
-        );
-        });
-    }
-    loadDocs() {
-      this.isLoadDocs = false;
-      this.mediaList = [];
+  public list() {
+    this._vehicleService.getCustomers().subscribe(data => {
+      this.customers = data.body;
       this.activatedRouter.paramMap.subscribe(params => {
         if (params.has('id')) {
           this._vehicleService.getVehicleById(params.get("id")).subscribe(data => {
-                this.mediaList = data?.body.media;
-                this.changeDetection.detectChanges();
-            })
+            this.vehicleDetail = data.body.id;
+            this.vehicleForm.patchValue(data.body);
+            this.vehicleForm.value.company_id = data.body.company.id;
+            this.vehicleForm.value.company = data.body.company.name;
+            this.mediaList = data?.body.media;
+            let x = 1;
+            console.log(333)
+            for (let index = 0; index < this.mediaList.length; index++) {
+              if(this.mediaList[this.mediaList.length - x]?.path.type == 'jpg' ||
+              this.mediaList[this.mediaList.length - x]?.path.type == 'png' ||
+              this.mediaList[this.mediaList.length - x]?.path.type == 'jpeg')
+              {
+              return this.imgsrc = `${environment.url}/media/s3/generateGetUrl?Key=` + this.mediaList[this.mediaList.length - x]?.path.key;
+              }
+              else
+              {
+                x++;
+              }
+            }
+          })
         };
       });
-      this.filesControl.setValue([]);
-      this.isLoadDocs = true;
-      this.isLoading = false;
-    }
+      this.filteredOptions = this.vehicleForm.controls['company'].valueChanges.pipe(
+        startWith(''),
+        map(value => this._filter(value === '' ? '99' : value))
+      );
+    });
+  }
+  loadDocs() {
+    this.isLoadDocs = false;
+    this.mediaList = [];
+    this.activatedRouter.paramMap.subscribe(params => {
+      if (params.has('id')) {
+        this._vehicleService.getVehicleById(params.get("id")).subscribe(data => {
+          this.mediaList = data?.body.media;
+          this.changeDetection.detectChanges();
+        })
+      };
+    });
+    this.filesControl.setValue([]);
+    this.isLoadDocs = true;
+    this.isLoading = false;
+  }
 
-    selectCustomer(event: any) {
-        let option = this.customers.filter(item => item.full_name.toUpperCase() === event.option.value.full_name.toUpperCase());
-        if (option.length > 0) {
-          this.selectCustomerItem = option[0];
-          this.vehicleForm.get('company_id').setValue(option[0].id, { emitEvent: false });
+  selectCustomer(event: any) {
+    let option = this.customers.filter(item => item.full_name.toUpperCase() === event.option.value.full_name.toUpperCase());
+    if (option.length > 0) {
+      this.selectCustomerItem = option[0];
+      this.vehicleForm.get('company_id').setValue(option[0].id, { emitEvent: false });
+    }
+  }
+  displayFn(x) {
+    return x.full_name;
+  }
+
+  deleteVehicle() {
+    if (this.vehicleDetail) {
+      this.dialogRef = this.dialog.open(ConfirmationDialog, {
+        disableClose: false
+      });
+      this.dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          this._vehicleService.deleteVehicle(this.vehicleDetail).subscribe();
         }
+        this.dialogRef = null;
+      });
     }
-    displayFn(x) {
-        return x.full_name;
-      }
-
-    deleteVehicle()
-    {
-        if(this.vehicleDetail)
-        {
-            this.dialogRef = this.dialog.open(ConfirmationDialog, {
-                disableClose: false
-              });
-              this.dialogRef.afterClosed().subscribe(result => {
-                if(result) {
-                    this._vehicleService.deleteVehicle(this.vehicleDetail).subscribe();
-                }
-                this.dialogRef = null;
-              });
+  }
+  deleteDocs(item) {
+    if (item) {
+      this.dialogRef = this.dialog.open(ConfirmationDialog, {
+        disableClose: false,
+      });
+      this.dialogRef.afterClosed().subscribe((result) => {
+        if (result) {
+          this.isLoading = true;
+          this._vehicleService.deleteDocs(item).subscribe(data => {
+            this.loadDocs();
+          })
         }
+        this.dialogRef = null;
+      });
     }
-    deleteDocs(item) {
-      if (item) {
-        this.dialogRef = this.dialog.open(ConfirmationDialog, {
-          disableClose: false,
-        });
-        this.dialogRef.afterClosed().subscribe((result) => {
-          if (result) {
-            this.isLoading = true;
-            this._vehicleService.deleteDocs(item).subscribe(data => {
-              this.loadDocs();
-            })
-          }
-          this.dialogRef = null;
-        });
-      }
-    }
+  }
 
-    public toggleStatus(): void {
-        this.filesControl.disabled
-          ? this.filesControl.enable()
-          : this.filesControl.disable();
-      }
-    
-      public toggleMultiple() {
-        this.multiple = !this.multiple;
-      }
-    
-      public clear(): void {
-        this.filesControl.setValue([]);
-      }
-    
-      upload(item) {
+  public toggleStatus(): void {
+    this.filesControl.disabled
+      ? this.filesControl.enable()
+      : this.filesControl.disable();
+  }
+
+  public toggleMultiple() {
+    this.multiple = !this.multiple;
+  }
+
+  public clear(): void {
+    this.filesControl.setValue([]);
+  }
+
+  upload(item) {
+    const file = this.demoForm.value.files[0];
+    this.fileService.putUrl(file).then((res) => {
+      const {
+        data: { putURL },
+      } = res;
+      let str = file.name;
+      var last = str.substring(str.lastIndexOf(".") + 1, str.length);
         this.isLoading = true;
-        const file = this.demoForm.value.files[0];
-        this.fileService.putUrl(file).then((res) => {
-          const {
-            data: { putURL },
-          } = res;
-    
-          this.fileService.upLoad(putURL, file).then(
-            (res) => {
-              this.toastr.successToastr(
-                this.translocoService.translate("message.fileUpload")
-              );
-              let str = file.name;
-              var last = str.substring(str.lastIndexOf(".") + 1, str.length);
-              let key = this.authService.user_id + '/'+ file.name;
-              let pathObject = {type:last, fileName:file.name, key:key, group: item.description}
-              this.mediaService
-                .createMedia({
-                  id: null,
-                  vehicle_id: this.vehicleDetail,
-                  title: "VehicleFile",
-                  user_id: this.authService.user_id,
-                  path: pathObject,
-                })
-                .subscribe((data) => {
-                  this.toastr.successToastr(
-                    this.translocoService.translate("message.createMedia"));
-                    this.loadDocs();
-                });
-            },
-            (error) => {
-              this.toastr.errorToastr(
-                this.translocoService.translate("message.error")
-              );
-              this.loadDocs();
-            }
-          );
-        });
-      }
+        this.fileService.upLoad(putURL, file).then(
+          (res) => {
+            this.toastr.successToastr(
+              this.translocoService.translate("message.fileUpload")
+            );
+            let key = this.authService.user_id + '/' + file.name;
+            let pathObject = { type: last, fileName: file.name, key: key, group: item.description }
 
-        /**
-        * Track by function for ngFor loops
-        *
-        * @param index
-        * @param item
-        */
-       trackByFn(index: number, item: any): any
-       {
-           return item.id || index;
-       }
-      clickFile(item)
-      {
-        this.fileDownloadLink = `${environment.url}/media/s3/generateGetUrl?Key=`+item?.path.key;
+            this.mediaService
+              .createMedia({
+                id: null,
+                vehicle_id: this.vehicleDetail,
+                title: "VehicleFile",
+                user_id: this.authService.user_id,
+                path: pathObject,
+              })
+              .subscribe((data) => {
+                this.toastr.successToastr(
+                  this.translocoService.translate("message.createMedia"));
+                this.loadDocs();
+              });
+          },
+          (error) => {
+            this.toastr.errorToastr(
+              this.translocoService.translate("message.error")
+            );
+            this.loadDocs();
+          }
+        );
+    });
+  }
+  uploadImg(item) {
+    const file = this.demoForm.value.files[0];
+    this.fileService.putUrl(file).then((res) => {
+      const {
+        data: { putURL },
+      } = res;
+      let str = file.name;
+      var last = str.substring(str.lastIndexOf(".") + 1, str.length);
+      if (last == 'jpg' || last == 'png' || last == 'jpeg') {
+        this.isLoading = true;
+        this.fileService.upLoad(putURL, file).then(
+          (res) => {
+            this.toastr.successToastr(
+              this.translocoService.translate("message.fileUpload")
+            );
+            let key = this.authService.user_id + '/' + file.name;
+            let pathObject = { type: last, fileName: file.name, key: key, group: item.description }
+
+            this.mediaService
+              .createMedia({
+                id: null,
+                vehicle_id: this.vehicleDetail,
+                title: "VehicleFile",
+                user_id: this.authService.user_id,
+                path: pathObject,
+              })
+              .subscribe((data) => {
+                this.toastr.successToastr(
+                  this.translocoService.translate("message.createMedia"));
+                this.loadDocs();
+                this.list();
+
+              });
+          },
+          (error) => {
+            this.toastr.errorToastr(
+              this.translocoService.translate("message.error")
+            );
+            this.loadDocs();
+            this.list();
+          }
+        );
       }
+      else {
+        this.toastr.errorToastr(
+          this.translocoService.translate("message.image"));
+      }
+    });
+  }
+
+  /**
+  * Track by function for ngFor loops
+  *
+  * @param index
+  * @param item
+  */
+  trackByFn(index: number, item: any): any {
+    return item.id || index;
+  }
+  clickFile(item) {
+    this.fileDownloadLink = `${environment.url}/media/s3/generateGetUrl?Key=` + item?.path.key;
+  }
 }
